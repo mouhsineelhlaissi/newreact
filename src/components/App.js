a5838200abd1cb81a1d4ed3b2cd27a9e223afe66
mouhsineelhlaissi <mouhsinesm@gmail.com>
2022-08-31 21:26:11 +0100

    zedt accolade

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './Header'
 import Text from './Text'
 import Social from './Social'
+import socialItem from './socialItem'
 function App()
 {
     return(
--- src/components/socialItem.js
+++ src/components/socialItem.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 function socialItem(mouhsine){
     return(
-        <li className=" list-group-item" ><input type="checkbox" id="mouhsine.media"  />
-        <label for="mouhsine.media">mouhsine.media</label>
+        <li className=" list-group-item" ><input type="checkbox" id="{mouhsine.media}"  />
+        <label for="{mouhsine.media}">{mouhsine.media}</label>
   </li>
       )
 }
